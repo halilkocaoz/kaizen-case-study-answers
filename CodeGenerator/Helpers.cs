478dc4f6b74b07ac8961b1300d8011dc06f3fa76
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-12-28 22:13:45 +0300

    implement generate code with cryptographic random number generator

--- CodeGenerator/Helpers.cs
+++ CodeGenerator/Helpers.cs
@@ -1,9 +1,15 @@
-﻿namespace CodeGenerator;
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CodeGenerator;
 
 public static class Helpers
 {
     public static string GenerateCode()
     {
-        return "";
+        var bytes = new byte[4];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(bytes);
+        return BitConverter.ToString(bytes).Replace("-", string.Empty).ToLower();
     }
 }
