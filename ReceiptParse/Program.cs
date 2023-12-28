b6b49d6a712ce6e052fb30919ca9e72344d9a6df
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-12-28 23:04:17 +0300

    Add ReceiptParse console

---
+++ ReceiptParse/Program.cs
@@ -0,0 +1,7 @@
+﻿var json = File.ReadAllText("receipt.json");
+var lines = ReceiptParser.GetLines(json);
+var txtBuilder = new System.Text.StringBuilder();
+foreach (var line in lines)
+    txtBuilder.AppendLine(line);
+
+File.WriteAllText("parsed.txt", txtBuilder.ToString());
---
+++ ReceiptParse/ReceiptParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class BoundingPoly
+{
+    [JsonPropertyName("vertices")]
+    public List<Vertex> Vertices { get; set; }
+}
+
+public class ReceiptItem
+{
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+    [JsonPropertyName("boundingPoly")]
+    public BoundingPoly BoundingPoly { get; set; }
+}
+
+public class Vertex
+{
+    [JsonPropertyName("x")]
+    public int X { get; set; }
+    [JsonPropertyName("y")]
+    public int Y { get; set; }
+}
+
+public static class ReceiptParser
+{
+    public static List<string> GetLines(string jsonString)
+    {
+        return new List<string>();
+    }
+}
